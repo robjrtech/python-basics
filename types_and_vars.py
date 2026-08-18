031fe4b9504bb53dc9e633c7a530743c7bb2160d
Robert Saunders Jr <robjrtech@gmail.com>
2026-08-18 13:16:51 -0500

    Fixed print statements for introduction

--- types_and_vars.py
+++ types_and_vars.py
@@ -2,11 +2,16 @@
 my_name = "Robert"
 my_age = 30
 my_height = 2
+width = 5.5
+height = 2
 
 
+# Prints a brief introduction
+print(f"Hello, my name {my_name} I am {my_age} years old and {my_height} meters tall.")
 
+# Prints your age in the next five years
+print(f"In 5 years, I will be {my_age + 5} years old")
 
-print(f"Hello, my name {name} I am {age} years old and {height} meters tall.")
-print(f"In 5 years, I will be {age + 5} years old")
-
+# Calculates the area of rectangle (w x h)
+print(f"The area of a {width} x {height} rectangle is {width * height}")
 
