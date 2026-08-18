7f659a98159ff64d2069d1cae046929e38aa2d7a
Robert Saunders Jr <robjrtech@gmail.com>
2026-08-18 14:47:03 -0500

    Completed part 2.1: String Fun

---
+++ string_fun.py
@@ -0,0 +1,3 @@
+fav_color = input("Type your favorite color: ")
+
+print(f"Your favorite color is {fav_color}. There are {len(fav_color)} letters to spell your favorite color! \n {fav_color.upper()}! \n" * 3)
