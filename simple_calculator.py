f246678e8edb905c6d252734430e35ca18599b85
Robert Saunders Jr <robjrtech@gmail.com>
2026-08-18 17:01:46 -0500

    Created an input validator for division

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,21 +1,20 @@
+import sys
+
 # Greeting
 print("Hello, I am a simple calculator who can handle basic arithmetic (+, -, *, /)")
 
 # First user input: num1
 num1 = input("Enter in your first number: ")
 
-# if num1 == int(num1):
-#     pass
-
 # Second user input: (+, -, *, /)
 symbol = input("Pick +, -, *, /: ")
 
 # Third user input: num2
 num2 = input("Enter in your second number: ")
-# if num2 == int(num2):
-#     pass
 
-
+if (num1 or num2 > 0) and symbol == "/":
+    print("Please use a number greater then zero")
+    sys.exit()
 
 # Figures out what symbol did the user pick: (+, -, *, /) and prints the equation and answer
 if symbol == "+":
