6040ffe346933d26096d2aee9f759b0afb34cf8a
Robert Saunders Jr <robjrtech@gmail.com>
2026-08-18 16:30:25 -0500

    Added comments to string_fun.py file

--- string_fun.py
+++ string_fun.py
@@ -1,3 +1,5 @@
+# Ask users' favorite color
 fav_color = input("Type your favorite color: ")
 
+# Prints user favorite color and length and switches the casing to uppercase
 print(f"Your favorite color is {fav_color}. There are {len(fav_color)} letters to spell your favorite color! \n {fav_color.upper()}! \n" * 3)
