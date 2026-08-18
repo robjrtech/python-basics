1d46daf11c35f2a2b8997b342e4ef5bd60f8eded
Robert Saunders Jr <robjrtech@gmail.com>
2026-08-18 13:07:46 -0500

    Updated the variable names

--- types_and_vars.py
+++ types_and_vars.py
@@ -1,8 +1,12 @@
 # Declared variables
+my_name = "Robert"
+my_age = 30
+my_height = 2
 
-name = "Robert"
-age = 30
-height = 2
+
 
 
 print(f"Hello, my name {name} I am {age} years old and {height} meters tall.")
+print(f"In 5 years, I will be {age + 5} years old")
+
+
